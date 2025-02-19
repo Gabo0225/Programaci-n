52818689aaae1104f5041f596bf6374a6510e7aa
gabo0225 <Gabrielsaher@hotmail.com>
2025-02-18 21:07:16 -0500

    Update prueba2_test.py

--- prueba2_test.py
+++ prueba2_test.py
@@ -3,14 +3,14 @@
 import prueba2
 
 def test():
-    """Unit test for do_helloworld function"""
+    """Unit test for hello function"""
     # run the function to test and recover its output
     check_output = prueba2.hello(f"{prueba2.nombre}")
     # verify the output
-    if check_output == f"Hola Gabriel, ¿Cómo estas?": 
+    if check_output == prueba2.hello(f"Hola {prueba2.nombre}, ¿Cómo estas?"): 
         print("Test is OK")
     else: 
-        print(f'"Test is not OK! expected a "Hola Gabriel, ¿Cómo estas?"')
+        print(f'"Test is not OK! expected a "Hola {prueba2.nombre}, ¿Cómo estas?"')
         print(f'.. but received {check_output}')
 
 if __name__ == "__main__":
